9ef57af299df6e63901767bf30591011eb9fb585
JayPatel504 <jpatel9000@gmail.com>
2021-11-30 01:02:48 -0500

    change input files and others

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from file.writeLog import Writing
 import os
 
-f_path = os.path.join(os.getcwd(),"input")
-print("running")
-while True:
-    if len(f_path) == 0:
-        continue
-    else:
-        for file in [f for f in os.listdir(f_path) if f.endswith('.csv')]:
-            fi=os.path.join(f_path,file)
-            Writing.writeL(fi)
-            os.replace(fi,os.path.join(os.getcwd(),"done",file))
+def main():
+    f_path = os.path.join(os.getcwd(),"input")
+    print("running")
+    while True:
+        if len(f_path) == 0:
+            continue
+        else:
+            for file in [f for f in os.listdir(f_path) if f.endswith('.csv')]:
+                fi=os.path.join(f_path,file)
+                Writing.writeL(fi)
+                os.replace(fi,os.path.join(os.getcwd(),"done",file))
+
+if __name__ == "__main__":
+    main()
--- tests/csv_test.py
+++ tests/csv_test.py
@@ -1,38 +1,14 @@
 """Testing the Calculator"""
 import pytest
 import pandas as pd
-
-from calculator.calculator import Calculator
+import main
 
 @pytest.fixture(name="clear_history",autouse=True)
 def fixture_clear_history():
     '''clear history function'''
     Calculator.clear_history()
 
-def test_add_number():
-    """Testing the Add function of the calculator"""
-    df = pd.read_csv('./files/addition1.csv').to_numpy()
-    for i in range(1,len(df)):
-        Calculator.add_number(tuple(df[i][:-1]))
-        assert Calculator.get_last_calculation().get_result() == df[i][-1]
+def test_logging():
+    main.logging()
 
-def test_subtract_numbers():
-    """Testing the subtract method of the calculator"""
-    df = pd.read_csv('./files/subtraction1.csv').to_numpy()
-    for i in range(1,len(df)):
-        Calculator.subtract_numbers(tuple(df[i][:-1]))
-        assert Calculator.get_last_calculation().get_result() == df[i][-1]
 
-def test_multiply_numbers():
-    """ tests multiplication of two numbers"""
-    df = pd.read_csv('./files/multiplication1.csv').to_numpy()
-    for i in range(1,len(df)):
-        Calculator.multiply_numbers(tuple(df[i][:-1]))
-        assert Calculator.get_last_calculation().get_result() == df[i][-1]
-
-def test_divide_numbers():
-    """ tests division of two numbers"""
-    df = pd.read_csv('./files/division1.csv').to_numpy()
-    for i in range(1,len(df)):
-        Calculator.divide_numbers(tuple(df[i][:-1]))
-        assert Calculator.get_last_calculation().get_result() == df[i][-1]
